b4a489edaf083c0cb910b8939490caacc701306f
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-09-13 00:58:51 +0300

    Update blockchain.js

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,1 +1,51 @@
+import Block from './block.js';
 
+class Blockchain{
+  constructor(){
+    this.difficulty = 2;
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    const genesisBlock = new Block("Genesis Block");
+    genesisBlock.index = 0;
+    genesisBlock.previousHash = "0012genesis345block678";
+    // genesisBlock.hash = genesisBlock.calculateHash();
+    genesisBlock.mineBlock(this.difficulty);
+    return genesisBlock;
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    if(!this.isChainValid()){
+      throw new Error('Blockchain is not valid');
+    }
+    newBlock.index = this.chain.length;
+    newBlock.previousHash = this.getLatestBlock().hash;
+    // newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for(let i = 1; i < this.chain.length; i++){
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i-1];
+
+      if(currentBlock.hash !== currentBlock.calculateHash()){
+        return false;
+      }
+
+      if(currentBlock.previousHash !== previousBlock.hash){
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+export default Blockchain;
