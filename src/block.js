f04c901665f92ca0d4f6b26c9bc5f816796805ff
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-09-13 00:58:03 +0300

    Update block.js

--- src/block.js
+++ src/block.js
@@ -1,1 +1,31 @@
+import CryptoJS from 'crypto-js';
+
+const SHA256 = CryptoJS.SHA256;
+
+class Block {
+  constructor(data) {
+    this.index = null;
+    this.timestamp = new Date().getTime().toString();
+    this.data = data;
+    this.previousHash = '';
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("Block mined: " + this.hash);
+    console.log("Block has length: " + this.hash.length);
+  }
+}
+
+export default Block;
 
